bb514a5e8b09b380e53736a04d1c10988eff5f72
Your Name <you@example.com>
2025-11-21 09:56:11 +0530

    (backend) authentication implemented

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export const protectRoute = async (req, res, next) => {
+    
+    try {
+        const token = req.cookies.jwt
+
+        if(!token) {
+            return res.status(401).json({message:"Unauthorized -  No token provided" })
+        }
+
+        const decode = jwt.verify(token, process.env.JWT_SECRET_KEY)
+
+        if(!decode) {
+            return res.status(401).json({message:"Unauthorized -  Invalid token" })
+        }
+
+        const user =  User.findById(decode.userId)
+
+    } catch(error) {
+
+    }
+
+}
---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,15 @@
+import express from "express"
+
+import { signup, login, logout, onboard } from "../controllers/auth.controller.js";
+
+const router = express.Router()
+
+router.post("/signup", signup)
+
+router.post("/login",login)
+
+router.post("/logout", logout)
+
+router.post("/onboarding", onboard)
+
+export default router
