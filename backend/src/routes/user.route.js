84bbe039ffe1c8bf78f108bd154685c5a928e23c
Your Name <you@example.com>
2025-11-22 10:44:54 +0530

    (backend)  dashboard api - friend request accept, pending implemented

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -5,6 +5,8 @@ import {
   getMyFriends,
   sendFriendRequest,
   acceptFriendRequest,
+  getFriendRequests,
+  getOutgoingFriendReqs,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
@@ -17,6 +19,10 @@ router.get("/friends", getMyFriends);
 
 router.post("/friend-request/:id", sendFriendRequest);
 
-router.post("/friend-request/:id/accept", acceptFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
+
+router.get("/friend-requests", getFriendRequests);
+
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
 
 export default router;
