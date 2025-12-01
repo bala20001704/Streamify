f1017811246d5b37ee77710406399ebfd9632a4f
Your Name <you@example.com>
2025-12-01 12:16:32 +0530

    (frotend) notifications

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,16 +1,16 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { getUserFriends, getRecommendedUsers, getOutgoingFriendReqs, sendFriendRequest } from "../lib/api";
 import { useEffect, useState } from "react";
+import { getOutgoingFriendReqs, getRecommendedUsers, getUserFriends, sendFriendRequest } from "../lib/api";
 import { Link } from "react-router";
-import { UserIcon, CheckCircleIcon, MapPinIcon, UserPlusIcon } from "lucide-react";
-import FriendCard from "../components/FriendCard";
-import NoFriendsFound from "../components/NoFriendsFound";
-import { getLanguageFlag } from "../components/FriendCard";
+import { CheckCircleIcon, MapPinIcon, UserPlusIcon, UsersIcon } from "lucide-react";
+
 import { capitialize } from "../lib/utils";
+
+import FriendCard, { getLanguageFlag } from "../components/FriendCard";
+import NoFriendsFound from "../components/NoFriendsFound";
 
 const HomePage = () => {
   const queryClient = useQueryClient();
-
   const [outgoingRequestsIds, setOutgoingRequestsIds] = useState(new Set());
 
   const { data: friends = [], isLoading: loadingFriends } = useQuery({
@@ -30,9 +30,7 @@ const HomePage = () => {
 
   const { mutate: sendRequestMutation, isPending } = useMutation({
     mutationFn: sendFriendRequest,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["outgoingFriendReqs"] });
-    },
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["outgoingFriendReqs"] }),
   });
 
   useEffect(() => {
@@ -49,24 +47,24 @@ const HomePage = () => {
     <div className="p-4 sm:p-6 lg:p-8">
       <div className="container mx-auto space-y-10">
         <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
-          <h2 className="text-2xl sm:text-3xl font-bold tracking-tighter">Your Friends</h2>
+          <h2 className="text-2xl sm:text-3xl font-bold tracking-tight">Your Friends</h2>
           <Link to="/notifications" className="btn btn-outline btn-sm">
-            <UserIcon className="mr-2 size-4" />
+            <UsersIcon className="mr-2 size-4" />
             Friend Requests
           </Link>
         </div>
 
         {loadingFriends ? (
           <div className="flex justify-center py-12">
-            <span className="loading loading-spinner loading-lg"></span>
+            <span className="loading loading-spinner loading-lg" />
           </div>
         ) : friends.length === 0 ? (
           <NoFriendsFound />
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-            {friends.map((friends) => {
-              <FriendCard key={friends._id} friends={friends} />;
-            })}
+            {friends.map((friend) => (
+              <FriendCard key={friend._id} friend={friend} />
+            ))}
           </div>
         )}
 
@@ -79,8 +77,6 @@ const HomePage = () => {
               </div>
             </div>
           </div>
-
-          {/*  */}
 
           {loadingUsers ? (
             <div className="flex justify-center py-12">
